0c40e7023a3c1f7d9b5cb9974024bfbcdbe18da6
Andy <voandy1998@gmail.com>
2018-03-27 21:37:44 +0000

    character count now red when over character limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $("#tweetfield").bind( "keyup", function() {
+    var charCount = $(this).val().length;
+    var counter = $(this).siblings("span.counter");
+
+    counter.text(140 - charCount);
+
+    if(charCount > 140) {
+      counter.css("color", "red");
+    } else {
+      counter.css("color", "black");
+    }
+  });
+
+});
