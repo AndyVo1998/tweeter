791d6af0e16a0167cfe53777708a8285c11c08fc
Andy <voandy1998@gmail.com>
2018-03-28 00:44:07 +0000

    hard coded tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  var article = document.createElement("article");
-}
+  function addTweet(content) {
+    if(content !== "") {
+      $("article").append("<p>" + content + "<p>");
+    }
+  }
+
+  function tweetSubmit(event) {
+    var input = $(event.target.elements.text);
+    addTweet(input.val());
+    debugger;
+  }
+
+  $("form").on("submit", tweetSubmit);
+});
