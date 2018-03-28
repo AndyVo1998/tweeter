724266760d33678d56479d0d8a0fe2ac11fd1fdc
Andy <voandy1998@gmail.com>
2018-03-28 02:57:38 +0000

    hover status

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,37 @@
-$(document).ready(function() {
-  function addTweet(content) {
-    if(content !== "") {
-      $("article").append("<p>" + content + "<p>");
-    }
-  }
+// $(document).ready(function() {
+//   var header = $(this).children("header");
+//   var h1 = $("header").children("h1");
+//   var username = $("header").children("h3");
+//   var img = $("header").children("img");
+//   // function addTweet(content) {
+//   //   if(content !== "") {
+//   //     $("article").append("<p>" + content + "<p>");
+//   //   }
+//   // }
 
-  function tweetSubmit(event) {
-    var input = $(event.target.elements.text);
-    addTweet(input.val());
-    debugger;
-  }
+//   // function tweetSubmit(event) {
+//   //   var input = $(event.target.elements.text);
+//   //   addTweet(input.val());
+//   //   debugger;
+//   // }
 
-  $("form").on("submit", tweetSubmit);
-});
+//   // $("form").on("submit", tweetSubmit);
+
+
+//   $("article").bind( "mouseover", function() {
+//     $(this).css({border: "1px solid black"});
+//     h1.css("color", "black");
+//     username.css("color", "black");
+//     img.css("opacity", "1");
+//   });
+
+//   $("article").bind( "mouseleave", function() {
+//     $(this).css({border: "1px solid #0000002e"});
+//     h1.css("color", "#244751a3");
+//     username.css("color", "#2447518a");
+//     img.css("opacity", "0.8");
+//   });
+// });
+
+
+// : 1px solid #0000002e;
